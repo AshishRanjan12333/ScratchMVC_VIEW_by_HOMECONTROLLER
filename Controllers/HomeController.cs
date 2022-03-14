d5a8f6d68788dae7a8b054a797bc88bedd2711db
A R <A R@LAPTOP-M0TJ75U0>
2022-03-15 01:12:47 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ScratchMVC.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScratchMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            Student ob = new Student()
+            {
+                Id = 1,
+              name = "Ashish RAnjan",
+              email="ashish@gmail.com",
+              roll=1
+            };
+            Student ob1 = new Student()
+            {
+                Id = 2,
+                name = " RAnjan",
+                email = "ashish@gmail.com",
+                roll = 2
+            };
+            Student ob2 = new Student()
+            {
+                Id = 3,
+                name = "Ashish ",
+                email = "ashish@gmail.com",
+                roll = 3
+            };
+            Student ob3 = new Student()
+            {
+                Id = 4,
+                name = "Ashish Raj",
+                email = "ashish@gmail.com",
+                roll = 4
+            };
+            List<Student> std = new List<Student>();
+            std.Add(ob);
+            std.Add(ob1);
+            std.Add(ob2);
+            std.Add(ob3);
+            return View(std);
+
+        } 
+        public IActionResult About()
+        {
+            return View();
+        }
+        
+    }
+}
---
+++ Model/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScratchMVC.Model
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public int roll { get; set; }
+    }
+}
